df3b9c093ba062554ca0f2f5dc6addbfaf65ca71
mlister3 <matthew.list56@outlook.com>
2023-04-03 19:04:03 -0400

    Update app.js

--- AppFilesJS/static/js/app.js
+++ AppFilesJS/static/js/app.js
@@ -11,22 +11,27 @@ function updatePlots(value) {
 function subjectSearch(value) {
     let subjectInfo = samples.find(obj => obj.id === value);
 
-    let otuIDs = subjectInfo.otu_ids;
-    console.log(otuIDs); //y
-
-    let sampleValues = subjectInfo.sample_values;
+    let sampleValues = subjectInfo.sample_values.slice(0, 10);
     console.log(sampleValues); //x
+
+    let otuIDs = subjectInfo.otu_ids.slice(0, 10);
+    console.log(otuIDs); //y
 
     let otuLabels = subjectInfo.otu_labels;
     console.log(otuLabels); 
+
+    barPlot(sampleValues, otuIDs);
 };
 
-function barPlot(value, xdata, ydata) {
+function barPlot(xdata, ydata) {
+
+    //let text = hoverData.map((xdata, index) => `hoverData: ${xdata}<br>y: ${y[index]}`);
 
     let trace = {
-        x: xdata,
-        y: ydata.map(id => `OTU ${ydata}`),
-        text: labels,
+        values: xdata,
+        labels: ydata.map(id => `OTU ${ydata}`),
+    //    text: text,
+    //    mode: "markers",
         type: "bar",
         orientation: "h"
     };
@@ -34,6 +39,6 @@ function barPlot(value, xdata, ydata) {
     let data = [trace];
 
     // Plot the chart
-    Plotly.newPlot("bar", data, layout);
+    Plotly.newPlot("bar", data);
     console.log("Bar Done.");
 };
