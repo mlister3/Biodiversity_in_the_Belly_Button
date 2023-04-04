70019e23c35254fd91ed6a5615ea1c0882e18f46
mlister3 <matthew.list56@outlook.com>
2023-04-03 23:46:58 -0400

    2nd graph complete

--- AppFilesJS/static/js/app.js
+++ AppFilesJS/static/js/app.js
@@ -12,15 +12,16 @@ function updatePlots(value) {
 
     let otuLabels = subjectInfo.otu_labels;
 
-    barPlot(sampleValues, otuIDs);
+    barPlot(sampleValues, otuIDs, otuLabels);
+    bubblePlot(otuIDs, sampleValues, otuLabels);
 };
 
-function barPlot(xdata, ydata) {
+function barPlot(xdata, ydata, hoverData) {
     
     let trace = {
         x: xdata,
         y: ydata.map(ydata => `OTU ${ydata}`),
-        text: ydata,
+        text: hoverData,
         type: "bar",
         orientation: "h"
     };
@@ -28,4 +29,25 @@ function barPlot(xdata, ydata) {
     let data = [trace];
 
     Plotly.newPlot("bar", data);
+};
+
+function bubblePlot(xdata, ydata, textData){
+    console.log(xdata, ydata, textData);
+    let trace2 = {
+        x: xdata,
+        y: ydata,
+        mode: 'markers',
+        type: 'scatter',
+        text: textData,
+        marker: {
+            color: xdata,
+            colorscale: 'Electric',
+            size: ydata
+        }
+    };
+
+    let data2 = [trace2];
+
+    Plotly.newPlot('bubble', data2);
+
 };
