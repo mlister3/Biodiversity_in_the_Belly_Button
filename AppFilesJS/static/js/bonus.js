48582cdf28fdf49a88f4c294d74a19e54af1170b
Matthew Lister <120275018+mlister3@users.noreply.github.com>
2023-04-09 15:42:33 -0700

    Update bonus.js

--- AppFilesJS/static/js/bonus.js
+++ AppFilesJS/static/js/bonus.js
@@ -1,25 +1,25 @@
 function gaugePlot() {
     let trace3 = {
         type: "indicator",
-        mode: "gauge",
         value: 3,
         title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
+        colorscales: "greens",
         gauge: {
             axis: { range: [null, 10], tickwidth: 1},
             bgcolor: "white",
             borderwidth: 2,
             bordercolor: "gray",
             steps: [
-                { range: [0, 1], color: "cyan" },
-                { range: [1, 2], color: ""},
-                { range: [2, 3], color: ""},
-                { range: [3, 4], color: ""},
-                { range: [4, 5], color: ""},
-                { range: [5, 6], color: ""},
-                { range: [6, 7], color: ""},
-                { range: [7, 8], color: ""},
-                { range: [8, 9], color: ""},
-                { range: [9, 10], color: "royalblue" }
+                { range: [0, 1]},
+                { range: [1, 2]},
+                { range: [2, 3]},
+                { range: [3, 4]},
+                { range: [4, 5]},
+                { range: [5, 6]},
+                { range: [6, 7]},
+                { range: [7, 8]},
+                { range: [8, 9]},
+                { range: [9, 10]}
             ],
         }
     };
