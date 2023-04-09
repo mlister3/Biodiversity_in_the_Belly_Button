4839f49f4573d981a4a7b077edf3d53e878154f3
Matthew Lister <120275018+mlister3@users.noreply.github.com>
2023-04-09 15:24:26 -0700

    Update bonus.js

--- AppFilesJS/static/js/bonus.js
+++ AppFilesJS/static/js/bonus.js
@@ -3,8 +3,7 @@ function gaugePlot(wash) {
         type: "indicator",
         mode: "gauge",
         value: wash,
-        title: { text: "Speed", font: { size: 24 } },
-        delta: { reference: 400, increasing: { color: "RebeccaPurple" } },
+        title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
         gauge: {
             axis: { range: [null, 500], tickwidth: 1, tickcolor: "darkblue" },
             bar: { color: "darkblue" },
@@ -12,8 +11,16 @@ function gaugePlot(wash) {
             borderwidth: 2,
             bordercolor: "gray",
             steps: [
-                { range: [0, 250], color: "cyan" },
-                { range: [250, 400], color: "royalblue" }
+                { range: [0, 1], color: "cyan" },
+                { range: [1, 2], color: ""},
+                { range: [2, 3], color: ""},
+                { range: [3, 4], color: ""},
+                { range: [4, 5], color: ""},
+                { range: [5, 6], color: ""},
+                { range: [6, 7], color: ""},
+                { range: [7, 8], color: ""},
+                { range: [8, 9], color: ""},
+                { range: [9, 10], color: "royalblue" }
             ],
             threshold: {
             line: { color: "red", width: 4 },
