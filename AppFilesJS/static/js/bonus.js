97ed0200af0814bcb02314435742047aac22a645
Matthew Lister <120275018+mlister3@users.noreply.github.com>
2023-04-09 17:06:08 -0700

    Update bonus.js

--- AppFilesJS/static/js/bonus.js
+++ AppFilesJS/static/js/bonus.js
@@ -1,11 +1,10 @@
 function gaugePlot(wf) {
+    // Define the trace data
     let trace4 = {
         type: "indicator",
         mode: "gauge",
         value: wf,
         title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
-        needle: "red",
-        colorscales: "greens",
         gauge: {
             axis: { range: [null, 10], tickwidth: 1},
             bgcolor: "white",
@@ -28,6 +27,39 @@ function gaugePlot(wf) {
 
     let data4 = [trace4];
 
-    Plotly.newPlot('gauge', data4);
+    let layout = {
+        shapes: [{
+            type: 'path',
+            path: 'M 0.5 0.52 L 0.5 0.82 L 0.52 0.8 Z',
+            fillcolor: 'red',
+            line: {
+                color: 'red'
+            },
+            xref: 'paper',
+            yref: 'paper',
+            x0: 0.45,
+            x1: 0.55,
+            y0: 0.25,
+            y1: 0.85
+        }]
+    };
 
-};
+    // Create the plot
+    let plot = Plotly.newPlot('gauge', data4, layout);
+
+    // Update the needle position based on wf
+    Plotly.update('gauge', {shapes: [{
+        type: 'path',
+        path: 'M 0.5 0.52 L 0.5 ' + (0.52 + 0.3 * wf / 10) + ' L 0.52 ' + (0.5 + 0.28 * wf / 10) + ' Z',
+        fillcolor: 'red',
+        line: {
+            color: 'red'
+        },
+        xref: 'paper',
+        yref: 'paper',
+        x0: 0.45,
+        x1: 0.55,
+        y0: 0.25,
+        y1: 0.85
+    }]});
+}
